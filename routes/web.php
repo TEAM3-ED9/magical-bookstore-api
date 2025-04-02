e5a21e6c3e33c1af40a993f9df2ac6f339025545
Juan Taborda <tabordajuan@gmail.com>
2025-04-02 16:56:05 -0400

    chore: add default path

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiInfoController;
 
-Route::get('/', [BookController::class, 'index']);
+Route::get('/', [ApiInfoController::class, 'index']);
+Route::get('/books', [BookController::class, 'index']);
